c91a55999ca62d70340b725dc308684caed75130
drdrej <ask@touchableheroes.com>
2017-05-08 23:55:21 +0200

    fix: onstart initialize now dialogbuinder

--- app/src/main/java/com/touchableheroes/drafts/spacerx/ui/DialogUtil.java
+++ app/src/main/java/com/touchableheroes/drafts/spacerx/ui/DialogUtil.java
@@ -1,18 +1,12 @@
 package com.touchableheroes.drafts.spacerx.ui;
 
 import android.app.Dialog;
-import android.content.ContentValues;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 
-import com.touchableheroes.drafts.spacerx.action.AbstractStateAction;
-
-import com.touchableheroes.drafts.spacerx.tx.StateTX;
-import com.touchableheroes.drafts.spacerx.ui.binding.AbstractUIBinder;
 import com.touchableheroes.drafts.spacerx.ui.binding.DialogBinder;
-import com.touchableheroes.drafts.spacerx.ui.binding.action.UIAction;
+
+import java.lang.reflect.Constructor;
 
 /**
  * Created by asiebert on 08.05.2017.
@@ -26,11 +20,11 @@ public class DialogUtil {
         private final Dialog dialog;
 
         public DialogBuilder(
-                final DialogBinder binder,
+                final Class<? extends DialogBinder> binderType,
                 final Context ctx) {
-            dialog = new Dialog(ctx) {
 
-                public DialogBinder dialog;
+            dialog = new Dialog(ctx) {
+                public DialogBinder dialogBinder;
 
                 @Override
                 protected void onCreate(
@@ -42,14 +36,23 @@ public class DialogUtil {
                 @Override
                 protected void onStart() {
                     super.onStart();
-                    this.dialog =  binder;
-                    this.dialog.bind();
+
+                    try {
+                        final Constructor<? extends DialogBinder> constructor = binderType.getConstructor(Dialog.class);
+                        dialogBinder = (DialogBinder) constructor.newInstance( this );
+                    } catch (final Throwable e) {
+                        e.printStackTrace();
+
+                        throw new IllegalStateException("dasdas", e);
+                    }
+
+                    this.dialogBinder.bind();
                 }
 
                 @Override
                 protected void onStop() {
                     super.onStop();
-                    this.dialog.destroy();
+                    this.dialogBinder.destroy();
                 }
             };
         }
@@ -71,7 +74,7 @@ public class DialogUtil {
         }
     }
 
-    public static DialogBuilder create(final DialogBinder binder,
+    public static DialogBuilder create(final Class<? extends DialogBinder> binder,
                                        final Context ctx) {
         return new DialogBuilder(binder, ctx);
     }
